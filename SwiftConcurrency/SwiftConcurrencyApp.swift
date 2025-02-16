ce7f909321d152349cdca9dd500f715e3e29bca6
Maulik shah <maulik_shah@hotmail.co.in>
2025-02-16 19:48:10 +0530

    First Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Maulik Shah on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
