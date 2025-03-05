4ed70925c0295e053c34189b4ed2401454938657
Maulik shah <maulik_shah@hotmail.co.in>
2025-03-05 20:55:47 +0530

    Actor Demo

--- SwiftConcurrency/ActorDemo.swift
+++ SwiftConcurrency/ActorDemo.swift
@@ -7,9 +7,136 @@
 
 import SwiftUI
 
+// 1. what is the problem that actor are solving
+// 2. how was this problem solved prior to actors?
+// 3. actor can solve the problem!
+
+
+class MYDataManager{
+    static let instance = MYDataManager()
+    
+    private init() {}
+    
+    var data : [String] = []
+    private let queue : DispatchQueue = .init(label: "com.example.myqueue")
+    
+    
+    func getRandomData(commplitionHandler: @escaping (_ title : String?) -> Void) {
+        queue.async {
+            self.data.append(UUID().uuidString)
+            
+            print(Thread.current)
+            commplitionHandler(self.data.randomElement())
+        }
+      
+    }
+}
+
+actor MyActorDataManager{
+    static let instance = MyActorDataManager()
+    
+    private init() {}
+    
+    var data : [String] = []
+    private let queue : DispatchQueue = .init(label: "com.example.myqueue")
+    
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        
+    print(Thread.current)
+       return self.data.randomElement()
+    }
+}
+
+
+struct HomeView : View {
+    
+    let manager = MyActorDataManager.instance
+    
+    @State private var text : String = ""
+    let timer =  Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        
+        ZStack{
+            Color.gray.opacity(0.8).ignoresSafeArea()
+            
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+//            DispatchQueue.global(qos: .background).async {
+//                manager.getRandomData { title in
+//                    if let title {
+//                        DispatchQueue.main.async{
+//                            self.text = title
+//                        }
+//                    }
+//                }
+//            }
+            
+            Task{
+                if  let data = await manager.getRandomData(){
+                    await MainActor.run {
+                        self.text = data
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+struct BrowserView : View {
+    
+    let manager = MyActorDataManager.instance
+
+    @State private var text : String = ""
+    let timer =  Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+   
+    var body: some View {
+        
+        ZStack{
+            Color.yellow.opacity(0.8).ignoresSafeArea()
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+//            DispatchQueue.global(qos: .background).async {
+//                manager.getRandomData { title in
+//                    if let title {
+//                        DispatchQueue.main.async{
+//                            self.text = title
+//                        }
+//                    }
+//                }
+//            }
+            
+            Task{
+                if  let data = await manager.getRandomData(){
+                    await MainActor.run {
+                        self.text = data
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ActorDemo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
+            BrowserView()
+                .tabItem {
+                    Label("Browse", systemImage: "magnifyingglass")
+                }
+        }
     }
 }
 
---
+++ SwiftConcurrency/GlobalActorDemo.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalActorDemo.swift
+//  SwiftConcurrency
+//
+//  Created by Maulik Shah on 3/5/25.
+//
+
+import SwiftUI
+
+struct GlobalActorDemo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GlobalActorDemo()
+}
